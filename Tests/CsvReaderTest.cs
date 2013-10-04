da5d664611aa56d444c3da89df9805e36b834645
izrik <izrik@yahoo.com>
2013-10-03 20:08:16 -0500

    Add a test for the csv reader.

---
+++ Tests/CsvReaderTest.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using System;
+using BeanCounter;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CsvReaderTest
+    {
+        [Test]
+        public void TestNoErrors()
+        {
+            // setup
+            string input = 
+                "line1,fielda,fieldb\n" +
+                "line2,fielda,fieldb,fieldc\n" +
+                "line3,fielda,fieldb";
+            var reader = new CsvReader();
+
+            // action
+            var lines = reader.ReadCsvFile(input);
+
+            // assertions
+            Assert.IsNotNull(lines);
+            Assert.AreEqual(3, lines.Count);
+
+            Assert.IsNotNull(lines[0]);
+            Assert.AreEqual(3, lines[0].Count);
+            Assert.AreEqual("line1", lines[0][0]);
+            Assert.AreEqual("fielda", lines[0][1]);
+            Assert.AreEqual("fieldb", lines[0][2]);
+
+            Assert.IsNotNull(lines[1]);
+            Assert.AreEqual(4, lines[1].Count);
+            Assert.AreEqual("line2", lines[1][0]);
+            Assert.AreEqual("fielda", lines[1][1]);
+            Assert.AreEqual("fieldb", lines[1][2]);
+            Assert.AreEqual("fieldc", lines[1][3]);
+
+            Assert.IsNotNull(lines[2]);
+            Assert.AreEqual(3, lines[2].Count);
+            Assert.AreEqual("line3", lines[2][0]);
+            Assert.AreEqual("fielda", lines[2][1]);
+            Assert.AreEqual("fieldb", lines[2][2]);
+        }
+    }
+}
+
