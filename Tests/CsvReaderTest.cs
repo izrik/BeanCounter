4d7d9f732114f93637c946c63e0427ed6e6100a6
izrik <izrik@yahoo.com>
2013-10-09 09:53:45 -0500

    Add tests for empty lines.

--- Tests/CsvReaderTest.cs
+++ Tests/CsvReaderTest.cs
@@ -191,6 +191,110 @@ namespace Tests
             Assert.AreEqual("fielda", lines[2][1]);
             Assert.AreEqual("fieldb", lines[2][2]);
         }
+
+        [Test]
+        public void TestEmptyRecord()
+        {
+            // setup
+            string input =
+                "line1,fielda,fieldb\n" +
+                "\n" +
+                "line3,fielda,fieldb";
+            var reader = new CsvReader();
+
+            // action
+            var lines = reader.ReadCsvFile(input);
+
+            // assertions
+            Assert.IsNotNull(lines);
+            Assert.AreEqual(3, lines.Count);
+
+            Assert.IsNotNull(lines[0]);
+            Assert.AreEqual(3, lines[0].Count);
+            Assert.AreEqual("line1", lines[0][0]);
+            Assert.AreEqual("fielda", lines[0][1]);
+            Assert.AreEqual("fieldb", lines[0][2]);
+
+            Assert.IsNotNull(lines[1]);
+            Assert.AreEqual(1, lines[1].Count);
+            Assert.AreEqual("", lines[1][0]);
+
+            Assert.IsNotNull(lines[2]);
+            Assert.AreEqual(3, lines[2].Count);
+            Assert.AreEqual("line3", lines[2][0]);
+            Assert.AreEqual("fielda", lines[2][1]);
+            Assert.AreEqual("fieldb", lines[2][2]);
+        }
+
+        [Test]
+        public void TestEmptyFirstRecord()
+        {
+            // setup
+            string input =
+                "\n" +
+                "line2,fielda,fieldb,fieldc\n" +
+                "line3,fielda,fieldb";
+            var reader = new CsvReader();
+
+            // action
+            var lines = reader.ReadCsvFile(input);
+
+            // assertions
+            Assert.IsNotNull(lines);
+            Assert.AreEqual(3, lines.Count);
+
+            Assert.IsNotNull(lines[0]);
+            Assert.AreEqual(1, lines[0].Count);
+            Assert.AreEqual("", lines[0][0]);
+
+            Assert.IsNotNull(lines[1]);
+            Assert.AreEqual(4, lines[1].Count);
+            Assert.AreEqual("line2", lines[1][0]);
+            Assert.AreEqual("fielda", lines[1][1]);
+            Assert.AreEqual("fieldb", lines[1][2]);
+            Assert.AreEqual("fieldc", lines[1][3]);
+
+            Assert.IsNotNull(lines[2]);
+            Assert.AreEqual(3, lines[2].Count);
+            Assert.AreEqual("line3", lines[2][0]);
+            Assert.AreEqual("fielda", lines[2][1]);
+            Assert.AreEqual("fieldb", lines[2][2]);
+        }
+
+        [Test]
+        public void TestEmptyLastRecord()
+        {
+            // setup
+            string input =
+                "line1,fielda,fieldb\n" +
+                "line2,fielda,fieldb,fieldc\n" +
+                "";
+            var reader = new CsvReader();
+
+            // action
+            var lines = reader.ReadCsvFile(input);
+
+            // assertions
+            Assert.IsNotNull(lines);
+            Assert.AreEqual(3, lines.Count);
+
+            Assert.IsNotNull(lines[0]);
+            Assert.AreEqual(3, lines[0].Count);
+            Assert.AreEqual("line1", lines[0][0]);
+            Assert.AreEqual("fielda", lines[0][1]);
+            Assert.AreEqual("fieldb", lines[0][2]);
+
+            Assert.IsNotNull(lines[1]);
+            Assert.AreEqual(4, lines[1].Count);
+            Assert.AreEqual("line2", lines[1][0]);
+            Assert.AreEqual("fielda", lines[1][1]);
+            Assert.AreEqual("fieldb", lines[1][2]);
+            Assert.AreEqual("fieldc", lines[1][3]);
+
+            Assert.IsNotNull(lines[2]);
+            Assert.AreEqual(1, lines[2].Count);
+            Assert.AreEqual("", lines[2][0]);
+        }
     }
 }
 
